78b61525db84aa7514eb1b2ae357d3ce35c3c9ef
manojkumarms25 <manojkumarms462@gmail.com>
2026-04-03 13:33:33 +0530

    adding driver

--- sync_fifo_uvm/driver.sv
+++ sync_fifo_uvm/driver.sv
@@ -10,4 +10,30 @@ class fifo_driver extends uvm_driver #(fifo_seq_item);
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     
+    if(!uvm_config_db #(virtual fifo_if.driver_mp)::get(this ,"","vif",vif))
+      `uvm_fatal("NO_VIF","drivwer,virtual interface not found");
+  endfunction
+  
+  
+  task run_phase(uvm_phase phase);
+    fifo_seq_item item;
+    vif.driver_cb.rst<=1;
+    vif.driver_cb.wr_en<=0;
+    vif.driver_cb.rd_en<=0;
+    vif.driver_cb.wr-data<=0;
     
+    
+    repeat(3)@(vif.driver_cb);
+    vif.driver_cb.rst<=0;
+    
+    forever begin
+      seq_item_port.get_next_item(item);
+      
+      @(vif.driver_cb);
+      
+      vif.driver_cb.wr_en<=item.wr_en;
+      vif.driver_cb.rd_en<=item.rd-en;
+      vif.driver_cb.wr_data<=item..wr_data;
+    end
+  endtask
+endclass
