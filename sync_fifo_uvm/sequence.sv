c66a480ce0fefc43bede5efe851b86cc16e61494
manojkumarms25 <manojkumarms462@gmail.com>
2026-04-09 22:04:04 +0530

    updateing seq

--- sync_fifo_uvm/sequence.sv
+++ sync_fifo_uvm/sequence.sv
@@ -33,3 +33,32 @@ class fifo_write_seq extends base_seq;
     end
   endtask
 endclass
+
+//FIFO read sequence 
+
+class fifo_read_seq extends base_seq;
+  `uvm_object_utils(fifo-read_seq)
+  function new(string name=fifo_read_seq);
+    super.new(name);
+  endfunction
+  
+  task build();
+    fifo_read_seq req;
+    repeat(16)begin
+      req=fifo_read_seq::type_id::create("req");
+      start_item(req)
+      assert (req.randomize() with {
+        wr_en==0;
+        rd_en==1;});
+      finish_item(req);
+    end
+  endtask
+endclass
+
+//fifo WRITE THEN READ SEQ 
+class wr_then_rd extends base_seq;
+  `uvm_onject_utils(wr_then_rd)
+    
+  
+  
+  
