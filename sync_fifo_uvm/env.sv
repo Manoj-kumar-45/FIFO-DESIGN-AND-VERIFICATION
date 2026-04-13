6a7d8fff7407fc7428b981115736183592853bbd
manojkumarms25 <manojkumarms462@gmail.com>
2026-04-13 21:55:37 +0530

    final env file

--- sync_fifo_uvm/env.sv
+++ sync_fifo_uvm/env.sv
@@ -5,26 +5,26 @@ class fifo_env extends uvm_env;
   fifo_scoreboard sb;
   
   
-  function new(string name ="fifo_env" uvm_component parent);
+  function new(string name ="fifo_env",uvm_component parent);
     super.new(name,parent);
   endfunction
   
   
-  //build phase 
+  //build phase //
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     
-    agt="fifo_agent"::type_id::create("agt",this);
-    sb="fifo_scoreboard"::type_id::create("sb",this);
+    agt=fifo_agent::type_id::create("agt",this);
+    sb=fifo_scoreboard::type_id::create("sb",this);
     
   endfunction 
   
-  //connect phase 
+  ////connect phase ////
   
   function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
     
-    agent.mon.analysis_port.connect(sb.analysis_export);
+    agt.mon.ap.connect(sb.analysis_export);
   endfunction
 endclass
 
