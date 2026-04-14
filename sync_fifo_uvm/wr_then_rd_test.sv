1b118e997a8671150d83acf282616841c0e391c5
manojkumarms25 <manojkumarms462@gmail.com>
2026-04-14 18:40:07 +0530

    adding write then read test file

---
+++ sync_fifo_uvm/wr_then_rd_test.sv
@@ -0,0 +1,21 @@
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+
+class wr_then_rd extends test;
+  
+  function new(string name="wr_then_rd",uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+  fifo_wr_then_rd_seq seq;
+  
+  task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    
+    seq=fifo_wr_then_rd_seq::type_id::create("seq");
+    seq.start(env.agt.seqr);
+    
+    phae.drop_objection(this);
+  endtask
+endclass
